a1e1bb862ef5278b3a1ab96709e460d092a452ab
saeed.falahat <a002066@c21054.ad.smhi.se>
2018-03-03 08:14:49 +0100

    new version splitstring is added

--- splitstring.c
+++ splitstring.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-char **split(char *st, char delim){
+char **split(char *st, char delim, int *NF){
   
   int nc= 0;
-
   char *temp = st;
 
   while(*temp != '\0'){
@@ -16,25 +15,24 @@ char **split(char *st, char delim){
       temp++;
     }
   }
-
-  printf("number of delimiter %c in string is %d\n",delim,nc);
-  
+  *NF = (nc+1);
+  //printf("number of delimiter %c in string is %d\n",delim,nc);
   char **cout = NULL;
-
+  int ni = 0;
+  int nj = -1;
+  char *tt = st;
   if (nc > 0){
     cout = (char **) malloc(sizeof(char *)*(nc+1));
-    int ni = 0;
     temp = st;
-    char *tt = st;
-    int nj = -1;
     while(*temp != '\0'){
       if ( *temp == delim){
 	nj++;
-	cout[nj] = (char *)malloc(sizeof(char) *ni);
+	cout[nj] = (char *)malloc(sizeof(char) *(ni+1));
 	for (int i=0; i <ni;++i){
 	  cout[nj][i] = *tt;
 	  tt++;
 	}
+	cout[nj][ni] = '\0';
 	ni=0;
 	temp++;
 	tt++;
@@ -47,44 +45,93 @@ char **split(char *st, char delim){
   }
 
   temp = st;
-  int nj = 0;
-  int ni = 0;
+  nj = 0;
+  ni = 0;
     
   while(*temp != '\0'){
       if ( *temp == delim){
 	nj++;
 	if (nj == nc){
-	  char *tt = temp;
+	  tt = temp;
 	  ni = 0;
 	  while(*++tt != '\0'){
 	    ++ni;
 	  }
-	  cout[nc] = (char*)malloc(sizeof(char)*ni);
+	  cout[nc] = (char*)malloc(sizeof(char)*(ni));
 	  tt = temp;
 	  ni = 0;
 	  while(*++tt != '\0'){
 	    cout[nc][ni] = *tt;
 	    ++ni;
 	  }
+	  cout[nc][ni-1] = '\0';
 	}
       }
       temp++;
   }
-  
   return cout;
 }
   
 int main(void){
   
-  char str[] ="time,lat,lon,lev,var,dim";
+  int NF = 0;
+  int NFF = 0;
 
-  char **cout = split(str,',');
+  char str[] ="t=time_counter&lat=latetitude&lon=longitude&z=level";
 
-  printf("%s\n",*cout);
-  printf("%s\n",*(cout+1));
-  printf("%s\n",*(cout+2));
-  printf("%s\n",*(cout+3));
-  printf("%s\n",*(cout+4));
-  printf("%s\n",*(cout+5));
+  char **cout = split(str,'&',&NF);
+
+  for (int i=0; i < NF;++i){
+    printf("%s\n",*(cout+i));
+    char **coutt = split(*(cout+i),'=',&NFF);
+    for (int j=0; j < NFF;++j){
+      printf("%s\t",*(coutt+j));
+    }
+    printf("\n");
+  }
+
+  FILE *fin = fopen("config.dat","r");
+  
+  int nc = 0;
+  while(!feof(fin)){
+    char c = fgetc(fin);
+    if (c == '\n') break;
+      nc++;
+  }
+
+  printf("%d\n",nc);
+  rewind(fin);
+
+  char *fcontent = (char*)malloc(sizeof(char)*(nc+1));
+
+  for (int i=0; i < nc;++i) fcontent[i] = '0';
+  fcontent[nc] = '\0';
+ 
+  nc = 0;
+  
+  while(!feof(fin)){
+    fcontent[nc] = fgetc(fin);
+    nc++;
+  }
+
+  fcontent[nc] = '\0';
+  
+  fclose(fin);
+
+  NF = 0;
+  NFF = 0;
+  cout = NULL;
+  cout = split(fcontent,'&',&NF);
+
+  printf("%d NF\n",NF);
+
+  for (int i=0; i < NF;++i){
+    char **coutt = split(*(cout+i),'=',&NFF);
+    for (int j=0; j < NFF;++j){
+      printf("%s\t",*(coutt+j));
+    }
+    printf("\n");
+  }
+
   return 0;
 }
