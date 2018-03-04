63e7d9d638c60a0c6c5b29d52f0b079f0e7ebda5
saeed81 <sd.falahat@gmail.com>
2018-03-04 18:13:38 +0100

    modification of mempool.c

--- mempool.c
+++ mempool.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define MEMSIZE (8)
+#define MEMSIZE (32)
 
 typedef struct{
   char *beg;
   char *end;
   char *cpool;
+  int cur;
 }Pool;
 
 
@@ -14,12 +15,14 @@ void getpool(Pool *pool, int np){
   pool->cpool = (char *) malloc(np*sizeof(char));
   pool->beg = pool->cpool;
   pool->end = (pool->cpool + np -1);
+  pool->cur = -1;
 }
 
 void destroypool(Pool *pool){
   if (pool->cpool != NULL )free(pool->cpool);
   pool->beg = NULL;
   pool->end = NULL;
+  pool->cur = 0;
 }
 
 char *push_pool(Pool *pool,int ni){
@@ -27,12 +30,13 @@ char *push_pool(Pool *pool,int ni){
   if ( pool->beg != NULL && ( (pool->end -pool->beg + 1) >= ni)){
     temp = pool->beg;
     pool->beg += ni;
+    pool->cur += ni;
   }
   return temp;
 }
 
-void print_address(char *pt, int nx){
-  char *temp = pt;
+void print_address(void *pt, int nx){
+  void *temp = pt;
   if (temp != NULL){
   for (int i=0; i < nx; i++){
     printf("%p\n",temp);
@@ -52,25 +56,23 @@ void print_values(char *pt, int nx){
   }
 }
 
-
-
-
 void pool_info(Pool *pool){
-  printf("beg = %p \t end = %p\t\n",pool->beg,pool->end);
+  printf("beg = %p \t end = %p\t cur= %d\n",pool->beg,pool->end,pool->cur);
 }
 
 int main(void){
 
-  Pool pool = {NULL,NULL,NULL};
-  getpool(&pool,MEMSIZE);  
-  pool_info(&pool);
-  print_address(pool.cpool,MEMSIZE);
+  Pool *pool =  (Pool *)malloc(sizeof(Pool));
+  pool->beg = NULL, pool->end =NULL, pool->cpool =NULL, pool->cur = 0;
+  getpool(pool,MEMSIZE);  
+  pool_info(pool);
+  print_address(pool->cpool,MEMSIZE);
 
   printf("\n");
 
   char *ar1 = NULL;
-  ar1 = push_pool(&pool,4);
-  pool_info(&pool);
+  ar1 = push_pool(pool,4);
+  pool_info(pool);
   print_address(ar1,4);
   
   *ar1 = 'a';
@@ -83,14 +85,36 @@ int main(void){
   printf("\n");
 
   char *ar2 = NULL; 
-  ar2 = push_pool(&pool,2);
+  ar2 = push_pool(pool,2);
   *ar2 = 'A';
   *(ar2+1) = 'B';
   print_values(ar2,2);
-  pool_info(&pool);
+  pool_info(pool);
   print_address(ar2,2);
  
-  destroypool(&pool);
+
+  int *ip = (int *)(push_pool(pool,16));
+  
+  ip[0] = 1;
+  ip[1] = 2000;
+  ip[2] = 30000;
+  ip[3] = 400000;
+
+  printf("%d\n",ip[0]);
+  printf("%d\n",ip[1]);
+  printf("%d\n",ip[2]);
+  printf("%d\n",ip[3]);
+
+  printf("%p\n",ip);
+  printf("%p\n",ip+1);
+  printf("%p\n",ip+2);
+  printf("%p\n",ip+3);
+
+  pool_info(pool);
+  
+  destroypool(pool);
+
+  free(pool);
 
   return 0;
 }
