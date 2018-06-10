1f871b5a56c926ff54aa220b887d0f5b779129f5
saeed81 <sd.falahat@gmail.com>
2018-06-10 19:23:32 +0200

    cat is modified

--- cat.c
+++ cat.c
@@ -8,21 +8,7 @@ int main(int argc, char *argv[]){
     printf("Usage: %s filename\n",argv[0]);
     return 1;
   }
-
   FILE *file = NULL;
-  #if 0
-  file = fopen(argv[1],"rb");
-  fseek(file,0,SEEK_END);
-  long int pos = ftell(file);
-  fseek(file,0,SEEK_SET);
-  char *content = (char *)malloc((pos+1));
-  content[pos] = '\0';
-  fread(content,1,pos,file);
-  fclose(file);
-  for(int i=0;i < pos;++i) printf("%c",content[i]);
-  free(content);
-  #endif
-  
   int ii = 1;
   char buffer[2048] = {'\0'};
   while (argc--){
@@ -34,10 +20,6 @@ int main(int argc, char *argv[]){
     while (fgets(buffer,2048,file) != NULL){
       printf("%s",buffer);
     }
-
-    //while ((c = fgetc(file)) != EOF){
-    //  printf("%c",c);
-    //}
 
     fclose(file);
 
