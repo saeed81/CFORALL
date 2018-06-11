abb6fd4cf319006a6e03efa9e4f7dbfe2308b527
saeed81 <sd.falahat@gmail.com>
2018-06-11 20:01:45 +0200

    array_value is added

--- arrayexplode.c
+++ arrayexplode.c
@@ -9,7 +9,7 @@ int power10(int n){
   }
   return a;
 }
-#if 0
+
 void fun(char *c){
   char *tmp = c;
   int len = 0;
@@ -29,9 +29,105 @@ void fun(char *c){
     printf("input character is digint\n");
     index = (c[0] - '0');
   }
-  if (index > 0) printf("%d\n",a[index]);
+  //if (index > 0) printf("%d\n",a[index]);
 }
-#endif
+
+void array_value(char *st, int ind){
+  int len   = 0;
+  char *tmp = st;
+  if (st == NULL) return;
+  while(*tmp != '\0'){
+    len++;
+    tmp++;
+  }
+  len++;
+  char type = '\0';
+  int dicel = 0;
+  if (st[0] == '['){
+    printf("this is array\n");
+    type = 'a';
+  }
+  int j = -1;
+  if (type == 'a'){
+    for (int i=1; i < len; ++i){
+      if (st[i] == '{'){
+	j = i;
+	break;
+      }
+    }
+  }
+  if (j > 0){
+    dicel = 1;
+    for (int i=(j-1); i>0;--i){
+      if ((st[i] != ' ') || (st[i] != '\t') || (st[i] != '\r') || (st[i] != '\n') ){
+	dicel = 0;
+	break;
+      }
+    }
+  }
+  int index = -1;
+  int icol  = -1;
+  int nel   = 0;
+  if (dicel == 1){
+    while( j < len){
+      icol = 0;
+      match(&st[j], 1, '{', &index);
+      //printf("j is %d and index is \t %c \n",j,st[j+index]);
+      for (int i=j;i<=(j+index);++i){
+	printf("%c",st[i]);
+      }
+      printf("\n");
+      nel++;
+      for (int i=(j+index+1);i < len;++i){
+	if (st[i] == ','){
+	  j = i;
+	  icol = 1;
+	  break;
+	}
+      }
+      if (icol == 0) break;
+      for (int i=(j+1);i < len;++i){
+	if (st[i] == '{'){
+	  j = i;
+	  break;
+	}
+      }
+    }
+    printf("number of elements is %d\n",nel);
+  }
+  index = -1;
+  icol  = -1;
+  nel   = 0;
+  j = 1;
+  int first = 1;
+  int last  = 1;
+  int stop = 1;
+  if (dicel == 0 ){
+    while( (j < len) && stop){
+      icol = 0;
+      for (int i=j;i < len;++i){
+	if (st[i] == ','){
+	  first = j;
+	  last = i;
+	  if (nel == ind){
+	    for (int k=first;k < last;++k) printf("%c",st[k]);
+	    printf("\n");
+	    stop = 1;
+	  }
+	  j = (i+1);
+	  icol = 1;
+	  break;
+	}
+      }
+      nel++;
+      //printf("j is %d\n",j);
+      if (icol == 0) break;
+    }
+    printf("number of elements is %d\n",nel);
+  }
+  //fun("10");
+  //printf("%d\n",power10(5));
+}
 
 void array_explode(char *st){
   int len   = 0;
@@ -120,15 +216,18 @@ void array_explode(char *st){
   //printf("%d\n",power10(5));
 }
 
+#if 1
 int main(void){
   
   char c[]  = "[{[{}]},{[{}]},{[{}]},{[{}]},{[{}]},{[{}]}]";
   char cd[] = "[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]";
   
-  array_explode(c);
-  array_explode(cd);
+  //array_explode(c);
+  //array_explode(cd);
+  array_value(cd,10);
+  
   
   return 0;
 
 }
-
+#endif
