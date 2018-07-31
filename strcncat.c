dcfe53f8ff36550b3696914a1707576f403a7c7d
saeed81 <sd.falahat@gmail.com>
2018-07-31 12:29:21 +0200

    modification

--- strcncat.c
+++ strcncat.c
@@ -8,54 +8,53 @@ typedef struct String{
   int counter;
 }String; 
 
-
-void String_init(String *vv){
-  vv->size = 0;
-  vv->counter = 0;
-  vv->aa = NULL;
+void String_init(String *str){
+  str->size = 0;
+  str->counter = 0;
+  str->aa = NULL;
   return;
 }
-void String_add(String *vv, char b){
-  if (vv->aa == NULL){
-    vv->size = 2;
-    vv->aa = (char *)malloc(vv->size * sizeof(char));
-    if (vv->aa == NULL) return;
-    vv->aa[vv->counter] = b;
-    vv->counter += 1;
+
+void String_add(String *str, char b){
+  if (str->aa == NULL){
+    str->size = 2;
+    str->aa = (char *)malloc(str->size * sizeof(char));
+    if (str->aa == NULL) return;
+    str->aa[str->counter] = b;
+    str->counter += 1;
   }
   else{
-    if (vv->counter < vv->size ){
-      vv->aa[vv->counter] = b;
-      vv->counter += 1;
+    if (str->counter < str->size ){
+      str->aa[str->counter] = b;
+      str->counter += 1;
     }
     else{
-      vv->size *= 2;
+      str->size *= 2;
       char *temp = NULL;
-      temp = (char *) realloc(vv->aa, vv->size * sizeof(char));
+      temp = (char *) realloc(str->aa, str->size * sizeof(char));
       if (temp == NULL) return;
-      vv->aa = temp;
-      vv->aa[vv->counter] = b;
-      vv->counter += 1;
+      str->aa = temp;
+      str->aa[str->counter] = b;
+      str->counter += 1;
     }
   }
 }
 
-void String_free(String *vv){
-  if (vv->aa != NULL)free(vv->aa);
-  vv->aa = NULL;
+void String_free(String *str){
+  if (str->aa != NULL)free(str->aa);
+  str->aa = NULL;
   return;
 }
 
-
-void String_dump(String *vv){
+void String_dump(String *str){
   printf("=============================================\n");
-  printf("String_size = %-8d String_counter = %-8d\n", vv->size, vv->counter);
-  if(vv->aa == NULL){
+  printf("String_size = %-8d String_counter = %-8d\n", str->size, str->counter);
+  if(str->aa == NULL){
     printf("input String is empty\n");
     printf("=============================================\n");
   }else{
-    for(int ii=0; ii < vv->counter; ++ii){
-      printf("%c",vv->aa[ii]);
+    for(int ii=0; ii < str->counter; ++ii){
+      printf("%c",str->aa[ii]);
     }
     printf("=============================================\n");
   }
@@ -101,7 +100,6 @@ char *String_concat(char *str1, char *str2){
     tmp++;
   }
   len12 = len1 + len2;
-  //printf("%u\n",len12);
   char *str12 = (char *)malloc((len12+1) *sizeof(char));
   char *strout = str12;
   if (str12 == NULL) return NULL;
@@ -109,14 +107,12 @@ char *String_concat(char *str1, char *str2){
   tmp = str1;
   while(*tmp != '\0' ){
     *str12 = *tmp;
-    //printf("%c\n",*str12);
     tmp++;
     str12++;
   }
   tmp = str2;
   while(*tmp != '\0'){
     *str12 = *tmp;
-    //printf("%c\n",*str12);
     tmp++;
     str12++;
   }
@@ -125,16 +121,17 @@ char *String_concat(char *str1, char *str2){
   return strout;
 }
 
-
 int main(int argc,char *argv[]){
- 
   char *name = NULL;
-  name = String_concat(argv[1],argv[2]);
+  name = String_concat("bb","cc");
   printf("%s\n",name);
   if (name != NULL) free(name);
-  String str;
-  String_concat_var(&str,"a","b","c","d","e","f","g","e","h",NULL);
-  String_dump(&str);
-  String_free(&str);
+  int i=10;
+  for(;i;--i){
+    String str;
+    String_concat_var(&str,"a","b","c","d","e","f","g","e","h","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",NULL);
+    String_dump(&str);
+    String_free(&str);
+  }
   return 0;
 }
