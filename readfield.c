d1ca8c139afea07206e35c8fdca426c60e49963f
saeed <sd.falahat@gmail.com>
2018-09-05 22:47:51 +0200

    good version of readfile

--- readfield.c
+++ readfield.c
@@ -6,10 +6,10 @@ int whitespace(char c){
   return 0;
 }
 
-int main(void){
-
+int main(int argc, char *argv[]){
+  if (argc != 2) return 1;
   FILE *fin = NULL;
-  fin = fopen("somedata","r");
+  fin = fopen(argv[1],"r");
   fseek(fin,0,SEEK_END);
   long int fs = ftell(fin);
   fseek(fin,0,SEEK_SET);
@@ -17,29 +17,38 @@ int main(void){
   content[fs] = '\0';
   fread(content,1,fs,fin);
   fclose(fin);
-  int nonwhite = 0;
-  for (long int i=0; i < fs;++i){
-    if (!whitespace(content[i])){
-      nonwhite++;
-    }
-    if (content[i] == '\n' ){
-      printf("%d number of nonwhite\n",nonwhite);
-      nonwhite = 0;
-    }
-  }
+  
   int nfield = 0;
   int nline = 0;
-  if (fs >= 2){ 
-    char *tmp = (char *)&content[1];
+  char *start;
+  char *end;
+  int found = 0;
+
+  if (fs >=1){ 
+    char *tmp = (char *)&content[0];
+    char *bef = tmp;
+    if (!whitespace(*bef)){ start = bef; found = 1;}
     while(*tmp != '\0'){
-      char *bef = tmp -1;
-      if (!whitespace(*bef) && whitespace(*tmp) ) nfield++;
-      if (*tmp == '\n'){
-	nline++;
-	printf("number of field in line %d is %d\n", nline,nfield);
-	nfield = 0;
+      if (!whitespace(*bef) && whitespace(*tmp)){
+	nfield++;
+	end = bef;
+	found = 0;
+	for (char *pt=start; pt <=end;++pt){printf("%c",*pt);}
+	printf(" ");
+	if (*tmp == '\n'){
+	  nline++;
+	  printf("number of field in line %d is %d\n", nline,nfield);
+	  nfield = 0;
+	  found = 0;
+	  printf("\n");
+	}
+      }
+      if (whitespace(*bef) && !whitespace(*tmp) && (found == 0)){
+	start = tmp;
+	found = 1;
       }
       tmp++;
+      bef = tmp -1;
     }
   }
   
