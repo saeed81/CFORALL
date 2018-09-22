9b17828554a31bbcd72435b7952033bc6f05ed55
saeed <sd.falahat@gmail.com>
2018-09-22 11:04:19 +0200

    strreplace is in good shape now

--- streplace.c
+++ streplace.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int lenstr(char *string){
+  int len = 0;
+  if (string == NULL) return len;
+  char *tmp = string;
+  while(*tmp!='\0'){
+    tmp++;
+    len++;
+  }
+  return len;
+}
+
+
 void stringreplace(char *src, char *sub, char *rep){
   
   if ((src == NULL) || (sub == NULL) || (rep == NULL))return;
@@ -51,107 +63,29 @@ void stringreplace(char *src, char *sub, char *rep){
     }
     t1++;
   }
-  
+
+  char *tmp  = src;
+  char *cout = src;
+  int ne = 0;
+  while (*tmp != '\0'){
+    if (*tmp != ' '){
+      *(cout + ne) = *tmp;
+      ne++;
+    }
+    tmp++;
+  }
+  *(cout+ne) = '\0';
   return;
 }
-
-
-
-void begendsubstring(char *src, char *sub, char **beg, char **end){
-  
-  if ((src == NULL) || (sub == NULL)){
-    *beg = NULL;
-    *end = NULL;
-    return ;
-  }
-  int len1 = 0, len2 = 0;
-  char *t1 = src;
-  char *t2 = sub;
-  while(*t1 != '\0'){
-    len1++;
-    t1++;
-  }
-  while(*t2 != '\0'){
-    len2++;
-    t2++;
-  }
-  if (len2 > len1 ){
-    *beg = NULL;
-    *end = NULL;
-    return ;
-  }
-  t1 = src;
-  t2 = sub;
-  int isexist = 0;
-  while(*t1 != '\0'){
-    t2 = sub;
-    if (*t1 == *t2){
-      isexist = 1;
-      *beg = t1; 
-      char *next =  t2+1;
-      char *ts   =  t1+1;
-      while (*next != '\0' && *ts != '\0'){
-	if (*next != *ts ){
-	  isexist = 0;
-	  break;
-	}
-	*end = ts;
-	next++;
-	ts++;
-      }
-    }
-    if (isexist) break;
-    t1++;
-  }
-
-  if (!isexist) {
-    *beg = NULL;
-    *end = NULL;
-  }
-
-  return;
-}
-
-void replace_imutable(char *str, char *sub, char *rep, char *target, int N){
-  char *beg = NULL;
-  char *end = NULL;
-  begendsubstring(str, sub, &beg, &end);
-  if (beg != NULL && end != NULL){
-    printf("we are here\n");
-  }
-  int ii = 0;
-  if (beg != NULL && end != NULL){
-    for (char *it=str; it <beg && ii < N ; ++it){
-      *target = *it;
-      target++;
-      ii++;
-    }
-  }
-  for (char *it=beg; (it <=end) && (ii < N) && (*rep != '\0'); ++it){
-    *target = *rep;
-    target++;
-    ii++;
-    rep++;
-  }
-  *target = '\0';
-}
-void replace(char *str, char *sub, char *rep){
-
-  if (str == NULL || sub == NULL || rep == NULL) return;
-  char *beg = NULL;
-  char *end = NULL;
-  begendsubstring(str, sub, &beg, &end);
-  if (beg == NULL || end== NULL) return; 
-  char *tmp = str;
-  for (char *it = beg; it <=end;++it) *it = ' ';
-}
-
 
 int main(void){
 
-  char src[] = "wind_speed_direction.json";
-  stringreplace(src, ".json", ".nc");
+  char src[] = "wind.json";
+  printf("size is %d \n",lenstr(src));
+  stringreplace(src, "json", "nc");
+  printf("size is %d \n",lenstr(src));
   stringreplace(src, "wind", "all");
+  printf("size is %d \n",lenstr(src));
   char *tmp = src;
   while(*tmp != '\0' ){
     printf("%c",*tmp);
